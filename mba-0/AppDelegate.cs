86a5a577af26df2e205ee694f39ade303e880842
Ted Shaffer <ted@brightsign.biz>
2016-04-08 08:12:43 -0700

    first commit

---
+++ mba-0/AppDelegate.cs
@@ -0,0 +1,40 @@
+﻿using AppKit;
+using Foundation;
+
+namespace mba0
+{
+	[Register ("AppDelegate")]
+	public class AppDelegate : NSApplicationDelegate
+	{
+		public AppDelegate ()
+		{
+		}
+
+		public override void DidFinishLaunching (NSNotification notification)
+		{
+			// Insert code here to initialize your application
+		}
+
+		[Export ("openDocument:")]
+		void OpenDialog (NSObject sender)
+		{
+			var dlg = NSOpenPanel.OpenPanel;
+			dlg.CanChooseFiles = true;
+			dlg.CanChooseDirectories = false;
+
+			if (dlg.RunModal () == 1) {
+				var alert = new NSAlert () {
+					AlertStyle = NSAlertStyle.Informational,
+					InformativeText = "At this point we should do something with the file that the user just selected in the Open File Dialog box...",
+					MessageText = "File Selected"
+				};
+				alert.RunModal ();
+			}
+		}
+		public override void WillTerminate (NSNotification notification)
+		{
+			// Insert code here to tear down your application
+		}
+	}
+}
+
